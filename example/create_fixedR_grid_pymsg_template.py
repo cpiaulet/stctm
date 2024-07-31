dadcad6ade69446b2ff5486ec6698679f4789899
cpiaulet <caroline.piaulet@umontreal.ca>
2024-07-31 17:57:15 -0500

    Updated template script to generate model grid

--- example/create_fixedR_grid_pymsg_template.py
+++ example/create_fixedR_grid_pymsg_template.py
@@ -4,43 +4,31 @@
 Created on Thu May 18 12:50:31 2023
 
 @author: caroline
-Create a grid of stellar models for stellar contamination analyses using pysynphot
+Create a grid of stellar models for stellar contamination analyses using pymsg
 """
 import numpy as np
-import matplotlib.pyplot as plt
 import os
-os.environ['CRDS_SERVER_URL'] = "https://jwst-crds.stsci.edu"
-os.environ['CRDS_PATH'] = "/home/caroline/crds_cache"
-os.environ['PYSYN_CDBS'] = "/home/caroline/trds"
 
-
-os.environ['MESASDK_ROOT'] = "/home/caroline/mesasdk"
-os.environ['MSG_DIR'] = "/home/caroline/msg-1.2"
-
-import pysynphot as S
 import pymsg as msg
-import astropy.constants as const
-import astropy.units as u
-import pdb
 import h5py
 
 import stellar_retrieval_utilities as sru 
 
 #%% Stellar parameters
 
-star_name = "TRAPPIST-1"
+star_name = "TRAPPIST_1"
 resPower_target = 10000
 
-if 1:
+if star_name =="TRAPPIST_1":
     # for TRAPPIST-1
     param = dict()
     param["Tphot"] = 2566. #K, Agol+20
     param["met"] = 0.040 #[Fe/H], Gillon+2017
     param["logg_phot"] = 5.2 # cgs, Agol+20
-    
+else:
+    raise ValueError(str(star_name)+" is not defined!")
 
 
-use_pymsg = True
 
 #%%
 print("Loading pymsg SpecGrid...")
@@ -73,16 +61,13 @@ wv_min_um = 0.2
 wv_max_um = 5.4
 
 # stellar models grid file path
-if star_name == "TRAPPIST_1":
-    stmodfile = "R"+str(resPower_target)+"_model_grids/"+star_name+"_pymsg.h5" 
- 
-        # StellarModelGridFilePath = self.scarletpath+'/'+'../other_models/StellarModels/CustomGrids/'+os.path.basename(self.StellarModelGridFile)+'_pymsg_withTgrid_{:g}_logg{:g}_{:g}_{:g}_{:g}_{:g}_{:g}.h5'.format(params['Teffstar'],logg_range[0], logg_range[1],params['metfe'],waveRange[0],waveRange[1],resolution) 
+stmodfile = "R"+str(resPower_target)+"_model_grids/"+star_name+"_pymsg.h5" 
 
 print("Grid parameter ranges:")
 print("logg:", logg_range)
 print("Teff:", Teff_range)
 
-wv_template_thisR, wv_template_edges_thisR = sru.make_wavelength_array(wv_min_um=wv_min_um, wv_max_um=wv_max_um, resPower=resPower_target, use_pymsg=use_pymsg)
+wv_template_thisR, wv_template_edges_thisR = sru.make_wavelength_array(wv_min_um=wv_min_um, wv_max_um=wv_max_um, resPower=resPower_target, use_pymsg=True)
 
 # check if the grid file exists
 if os.path.exists(stmodfile):
